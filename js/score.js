7325b5b5dc648e3adc032fabaa0469e2b302cbb4
anzudevelop <72302256+anzudevelop@users.noreply.github.com>
2024-10-11 19:23:41 +0700

    test

--- js/score.js
+++ js/score.js
@@ -5,31 +5,27 @@ const scoreapp = new Vue({
         windowsEnum: {score: 0, players: 1, settings: 2},
         isGame: false,
         roundsCount: 4,
-        players: [
-            {name: 'Леша', score: [5, 1, 12, 1]},
-            {name: 'Ира', score: [3, 1, 3, 0]},
-            {name: 'Андрей', score: [5, 5, 2, 0]},
-            {name: 'Дыма', score: [1, 2, 13, 0]},
-        ],
+
+        players: ['Леша', 'Ира', 'Андрей', 'Дыма'],
+        scores: [[5, 1, 12, 1], [3, 1, 3, 0], [5, 5, 2, 0], [1, 2, 13, 0]],
         selectedColumn: null,
     },
     methods: {
         newGame() {
-            this.players.forEach(player => player.score = [])
+            this.scores.forEach(score => score = [])
             this.roundsCount = 0
             this.selectedColumn = null
         },
         addRound() {
-            this.players.forEach(player => player.score.push(0))
+            this.scores.forEach(score => score.push(0))
             this.roundsCount++
         },
         removeRound() {
             if (this.selectedColumn === null) return
             if (this.selectedColumn > this.roundsCount) return
-            this.players.forEach(player => player.score.splice(this.selectedColumn - 1, 1))
+            this.scores.forEach(score => score.splice(this.selectedColumn - 1, 1))
             this.roundsCount--
             this.selectedColumn = null
         },
-    }
-
+    },
 })
