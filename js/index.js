de730493b301e75cb21a17c39614f7ce5519cfa5
anzudevelop <72302256+anzudevelop@users.noreply.github.com>
2024-10-11 19:27:50 +0700

    fix

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const app = new Vue({
         user: null,
     },
     beforeMount() {
-        this.user = tg.initDataUnsafe.user
+        //this.user = tg.initDataUnsafe.user
         this.user = null //----------------
     },
     methods: {
@@ -31,17 +31,17 @@ const app = new Vue({
 
 })
 
-tg.expand(); //расширяем на все окно  
+// tg.expand(); //расширяем на все окно  
 
-tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
-tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
-tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
+// tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+// tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+// tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+// tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+// tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
 
-Telegram.WebApp.onEvent('mainButtonClicked', function(){
-	tg.sendData("some string that we need to send"); 
-	//при клике на основную кнопку отправляем данные в строковом виде
-});
+// Telegram.WebApp.onEvent('mainButtonClicked', function(){
+// 	tg.sendData("some string that we need to send"); 
+// 	//при клике на основную кнопку отправляем данные в строковом виде
+// });
 
 
--- js/score.js
+++ js/score.js
@@ -5,25 +5,28 @@ const scoreapp = new Vue({
         windowsEnum: {score: 0, players: 1, settings: 2},
         isGame: false,
         roundsCount: 4,
-
-        players: ['Леша', 'Ира', 'Андрей', 'Дыма'],
-        scores: [[5, 1, 12, 1], [3, 1, 3, 0], [5, 5, 2, 0], [1, 2, 13, 0]],
+        players: [
+            {name: 'Леша', score: [5, 1, 12, 1]},
+            {name: 'Ира', score: [3, 1, 3, 0]},
+            {name: 'Андрей', score: [5, 5, 2, 0]},
+            {name: 'Дыма', score: [1, 2, 13, 0]},
+        ],
         selectedColumn: null,
     },
     methods: {
         newGame() {
-            this.scores.forEach(score => score = [])
+            this.players.forEach(player => player.score = [])
             this.roundsCount = 0
             this.selectedColumn = null
         },
         addRound() {
-            this.scores.forEach(score => score.push(0))
+            this.players.forEach(player => player.score.push(0))
             this.roundsCount++
         },
         removeRound() {
             if (this.selectedColumn === null) return
             if (this.selectedColumn > this.roundsCount) return
-            this.scores.forEach(score => score.splice(this.selectedColumn - 1, 1))
+            this.players.forEach(player => player.score.splice(this.selectedColumn - 1, 1))
             this.roundsCount--
             this.selectedColumn = null
         },
