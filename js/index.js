f9d6566717fef7b3f0e0731eef4837be9fce5707
anzudevelop <72302256+anzudevelop@users.noreply.github.com>
2025-09-22 14:35:29 +0700

    test

--- js/index.js
+++ js/index.js
@@ -14,15 +14,3 @@ let app = new Vue({
 
   },
 })
-
-
-document.addEventListener('touchstart', function (event) {
-  if (event.target.matches('.slick-element')) { // "Быстрее, чем молния!"
-    event.target.classList.add('feeling-touchy');
-  }
-});
-document.addEventListener('touchend', function (event) {
-  if (event.target.matches('.slick-element')) { // "Время отпустить, DJ JavaScript на связи!"
-    event.target.classList.remove('feeling-touchy');
-  }
-});
--- js/tg.js
+++ js/tg.js
@@ -14,7 +14,7 @@ if (tg) {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ initData })
-  }).then(r => r.json()).then(console.log);
+  }).then(r => app.data2 = r.json())
 } else {
   console.log('Не в Telegram WebApp — можно использовать Telegram Login Widget.');
 }
