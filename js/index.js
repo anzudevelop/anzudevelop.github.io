e037207151d74d1da648eb05cd447ad15ce5b9a8
anzudevelop <72302256+anzudevelop@users.noreply.github.com>
2025-09-26 20:13:07 +0700

    t

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
-let app = new Vue({
-  el: '#app',
-  data() {
-    return {
-      page: 0,
-      pageEnum: { main: 0, newEvent: 1, garage: 2, eventList: 3, info: 4 },
-      tgUser: null,      // сюда пробросим безопасные данные
-      tgInitData: '',    // сырые initData для сервера
-    }
+Vue.createApp({
+  data: () => ({
+    page: 0,
+    pageEnum: { main: 0, newEvent: 1, garage: 2, eventList: 3, info: 4 },
+    tgUser: null,      // сюда пробросим безопасные данные
+    tgInitData: null,    // сырые initData для сервера
+  }),
+  methods: {
+
+  },
+  computed: {
+    // evenItems() {
+    //   return this.items.filter(i => i % 2 === 0)
+    // }
   },
   mounted() {
     const tg = window.Telegram?.WebApp;
 
     if (tg) {
       this.tgUser = tg.initDataUnsafe?.user || null;
-      this.tgInitData = tg.initData;
+      this.tgInitData = tg.initData || null;
 
       fetch('http://77.221.140.74:3000/telegram/init', {
         method: 'POST',
@@ -32,7 +37,7 @@ let app = new Vue({
       console.log('Не в Telegram WebApp — fallback');
     }
   },
-  methods: {
-    // любые методы, которые используют this.tgUser
-  },
-})
+  watch: {
+
+  }
+}).mount("#app")
